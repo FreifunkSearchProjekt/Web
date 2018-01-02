c2a1fd389fdcba48aa51d1dea243044404bfcc8c
MTRNord <mtrnord1@gmail.com>
2018-01-02 22:22:56 +0100

    Initial Website
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

---
+++ src/app/search-form/search-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.css']
+})
+export class SearchFormComponent implements OnInit {
+  public communities: any[] = [{"communityCode":"ffslfl","communityName":"Freifunk Schleswig-Flensburg"}];
+
+  public searchForm = this.fb.group({
+    communityID: [null, Validators.required],
+    search: [null, Validators.required]
+  });
+
+  constructor(public fb: FormBuilder, private http: HttpClient) { }
+
+  doSearch() {
+    const hostDomain = window.location.hostname;
+    const url = 'http://'+hostDomain+':9999/clientapi/search/'+this.searchForm.controls.communityID.value+'/'+this.searchForm.controls.search.value;
+    this.http.get(url).subscribe(data => {
+      console.log(data.hits);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
