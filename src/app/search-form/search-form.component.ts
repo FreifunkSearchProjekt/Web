4146f1eeebef6c566b3efbb55bbb8d45a847a37e
MTRNord <mtrnord1@gmail.com>
2018-01-14 22:26:00 +0100

    Intelligent Pagination
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -16,6 +16,12 @@ export class SearchFormComponent implements OnInit {
   public hits: HitsEntity[];
   public searching: boolean = false;
   public progress: number = 0;
+  public maxResults: number = 0;
+  private hostDomain: string;
+  private communityID: string;
+  private value: string;
+  private url: string;
+
   // pager object
   pager: any = {};
 
@@ -33,62 +39,32 @@ export class SearchFormComponent implements OnInit {
     this.hits = [];
     this.pagedItems = [];
     this.progress = 0;
+    this.maxResults = 0;
 
-    const hostDomain = this.config.getConfig("hostDomain");
-    let communityID = this.searchForm.controls.communityID.value || this.config.getConfig("defaultCommunityID");
-    const value = this.searchForm.controls.search.value;
+    this.hostDomain = this.config.getConfig("hostDomain");
+    this.communityID = this.searchForm.controls.communityID.value || this.config.getConfig("defaultCommunityID");
+    this.value = this.searchForm.controls.search.value;
 
-    if (!value) {
+    if (!this.value) {
       return
     }
 
-    const url = 'http://'+hostDomain+':9999/clientapi/search/'+communityID+'/'+value;
+    this.url = 'http://'+this.hostDomain+':9999/clientapi/search/'+this.communityID+'/'+this.value;
 
     this.searching = true;
 
-    const req = new HttpRequest('GET', url, {
-      reportProgress: true,
+    const Maxreq = new HttpRequest('GET', this.url+'/max/', {
+      reportProgress: false,
     });
 
-    this.http.request(req)
+    this.http.request(Maxreq)
       .subscribe(event => {
         switch (event.type) {
-          // handle the download progress event received
-          case HttpEventType.DownloadProgress:
-            this.progress = 100 * event.loaded / event.total;
-            break;
-
-          // handle the response event received
           case HttpEventType.Response:
-            // When getting the full response body
-            const data:SearchResponseRootObject = <SearchResponseRootObject>event.body;
-            this.hits = data.hits;
-            if (this.hits.length === 0) {
-              let fakeResult: HitsEntity = <HitsEntity>{};
-              fakeResult.fields = <Fields>{};
-              fakeResult.fields.title = "No Results Found";
-              fakeResult.fields.description = "Please Enter another Search Term.";
-              this.hits.push(fakeResult);
-            }
-            this.searching = false;
+            this.maxResults = <number>event.body;
             // initialize to page 1
             this.setPage(1);
-            break;
         }
-      },
-      error => {
-        if (error.status !== 404) {
-          console.error("Got Error while searching: " + JSON.stringify(error, null, 4));
-          let fakeResult: HitsEntity = <HitsEntity>{};
-          fakeResult.fields = <Fields>{};
-          fakeResult.fields.title = "Got Error While Searching";
-          fakeResult.fields.description = JSON.stringify(error, null, 4);
-          this.hits = [];
-          this.hits.push(fakeResult);
-        }
-        // initialize to page 1
-        this.setPage(1);
-        this.searching = false;
       });
   }
 
@@ -96,15 +72,59 @@ export class SearchFormComponent implements OnInit {
   }
 
   setPage(page: number) {
+    console.log(this.maxResults)
     if (page < 1 || page > this.pager.totalPages) {
       return;
     }
 
     // get pager object from service
-    this.pager = this.pagerService.getPager(this.hits.length, page);
+    this.pager = this.pagerService.getPager(this.maxResults, page);
+
+    this.searching = true;
+    const req = new HttpRequest('GET', this.url+'/'+this.pager.startIndex+'/', {
+      reportProgress: true,
+    });
+
+    this.http.request(req)
+    .subscribe(event => {
+      switch (event.type) {
+        // handle the download progress event received
+        case HttpEventType.DownloadProgress:
+          this.progress = 100 * event.loaded / event.total;
+          break;
+
+        // handle the response event received
+        case HttpEventType.Response:
+          // When getting the full response body
+          const data:SearchResponseRootObject = <SearchResponseRootObject>event.body;
+          this.pagedItems = data.hits;
+          if (this.pagedItems.length === 0) {
+            let fakeResult: HitsEntity = <HitsEntity>{};
+            fakeResult.fields = <Fields>{};
+            fakeResult.fields.title = "No Results Found";
+            fakeResult.fields.description = "Please Enter another Search Term.";
+            this.pagedItems.push(fakeResult);
+          }
+          this.searching = false;
+          break;
+      }
+    },
+      error => {
+        if (error.status !== 404) {
+          console.error("Got Error while searching: " + JSON.stringify(error, null, 4));
+          let fakeResult: HitsEntity = <HitsEntity>{};
+          fakeResult.fields = <Fields>{};
+          fakeResult.fields.title = "Got Error While Searching";
+          fakeResult.fields.description = JSON.stringify(error, null, 4);
+          this.pagedItems = [];
+          this.pagedItems.push(fakeResult);
+        }
+
+        this.searching = false;
+      });
 
     // get current page of items
-    this.pagedItems = this.hits.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    //this.pagedItems = this.hits.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 
 }
